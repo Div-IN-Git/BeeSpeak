b9ef99269d0597fc4bc6b959e4c6777efc760805
Divyanshu Swain <97213799+Div-IN-Git@users.noreply.github.com>
2026-02-05 18:35:11 +0530

    Update transliterator.py

--- language/transliterator.py
+++ language/transliterator.py
@@ -1,4 +1,13 @@
-# language/tamil.py
+# language/transliterate.py
+from indic_transliteration import sanscript
+from indic_transliteration.sanscript import transliterate
 
-def normalize(text: str) -> str:
-    return text.lower().strip()
+def transliterate_to_native(text: str, lang: str) -> str:
+    if lang == "ROMANIZED_HINDI":
+        return transliterate(text, sanscript.ITRANS, sanscript.DEVANAGARI)
+
+    if lang == "ROMANIZED_TAMIL":
+        return transliterate(text, sanscript.ITRANS, sanscript.TAMIL)
+
+    return text
+
